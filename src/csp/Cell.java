24c3cb53a3e78ae70fc44845d32df2aae7e9aa69
Enan Ajmain <3nan.ajmain@gmail.com>
2020-11-28 11:51:06 +0600

    Add in-code docs

--- src/csp/Cell.java
+++ src/csp/Cell.java
@@ -2,9 +2,27 @@ package csp;
 
 import java.util.ArrayList;
 
+/**
+ * Holds the position,value, and domain
+ * of a cell in a Latin Square.
+ * <p>
+ *
+ */
 public class Cell {
     int row;
     int col;
     int val;
+
+    /**
+     * A list of domain for this cell.
+     * <p></p>
+     * <ol>
+     *     <li>The domain of an assigned cell is the value that cell holds.
+     *     <li>The domain of an unassigned cell
+     *     is all the valid values that cell can be assigned.
+     *     <li>A value is valid for an unassigned cell if no other cell
+     *     in the same row or same col of this cell holds that value.
+     * <ol>
+     */
     ArrayList<Integer> possVals;
 }
